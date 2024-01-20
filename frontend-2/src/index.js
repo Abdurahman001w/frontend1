295fc127ffa5df6971b954305fb90c570ecf68af
abdurahman <atger@gmail.com>
2024-01-20 15:25:53 +0300

    frontend-2-2

---
+++ frontend-2/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Logo from "./logo";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <Logo />
+  </div>
+);
